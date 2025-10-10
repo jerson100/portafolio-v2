d27a864496f68fcf0fa125266db139b3f62f71a4
Jerson Omar Ramírez Ortiz <jramiror@emeal.nttdata.com>
2025-10-10 03:30:28 -0500

    feature(meta): Added description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,8 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Jerson Omar Ramírez Ortiz",
-  description: "Portafolio de Jerson Omar Ramírez Ortiz",
+  description:
+    "Desarrollador de software con más de 3 años de experiencia creando soluciones web, móviles y de escritorio. Me especializo en construir productos funcionales, escalables y centrados en el usuario, participando en todo el ciclo de desarrollo.",
 };
 
 const HomePage = () => {
